33cfe00518b49bb3c4fd6a80081115870e46e1ce
git1 <ouko.sherman@gmail.com>
2020-01-23 14:03:06 +0300

    Spring MVC Demo

---
+++ DemoMvc/src/main/java/com/shermanouko/AddController.java
@@ -0,0 +1,30 @@
+package com.shermanouko;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shermanouko.service.AddService;
+
+@Controller
+public class AddController {
+
+	@RequestMapping("/add")
+	public ModelAndView add(@RequestParam("t1")int i, @RequestParam("t2")int j, HttpServletRequest request, HttpServletResponse response) {
+		
+		AddService as = new AddService();
+		int k = as.Add(i, j);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display");
+		mv.addObject("result", k);
+		
+		
+		return mv;
+	}
+}
+
---
+++ DemoMvc/src/main/java/com/shermanouko/service/AddService.java
@@ -0,0 +1,8 @@
+package com.shermanouko.service;
+
+public class AddService {
+
+	public int Add(int i, int j) {
+		return i + j;
+	}
+}
